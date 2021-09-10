101c1f98be7a8125abbfe060f6f71a20dc3516f1
mzbn <84735489+mzbn@users.noreply.github.com>
2021-09-10 15:26:37 +0300

    Add files via upload

---
+++ js/script-slick.js
@@ -0,0 +1,13 @@
+
+// SLICK SLIDER
+$(document).ready(function() {
+    $('.main__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+      });
+});
